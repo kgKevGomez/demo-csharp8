e8c98e3485b2d8e52f23d71800d2cc4a11f3138e
Kevin Gómez <kevin.gomezhernandez@coolblue.nl>
2020-04-07 21:53:19 +0200

    demo

---
+++ DemoApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Log(new DemoClass{ Id = 0}));
+            Console.ReadLine();
+        }
+
+        static string Log(DemoClass demoClass) => demoClass switch
+        {
+            { Id: 1 } => "1",
+            _ => "No match"
+        };
+    }
+
+    public class DemoClass
+    {
+        public long Id { get; set; }
+    }
+
+    public struct DemoStruct
+    {
+        public long Id { get; set; }
+        public long NextId => Id + 1;
+        public override readonly string ToString() => NextId.ToString();
+    }
+}
